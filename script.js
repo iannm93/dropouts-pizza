614e6f185bdb0c87d86735c591ee2030ccd6e665
Ian Harris <ianmharris93@gmail.com>
2022-07-14 18:15:17 -0700

    added dynamic text aligning for varying lengths of reviews from google/yelp

--- script.js
+++ script.js
@@ -2,56 +2,98 @@ let gallery = document.querySelector("#gallery");
 let galleryImage = document.querySelector(".galleryImage");
 let imageTarget = document.getElementById("img-target");
 let reviewBody = document.getElementById("review-body")
+let starContainer = document.getElementById("star-container")
+let nameTarget = document.getElementById("name-target")
 
 let reviews = [
   {
     text: "We attended my sons all day wrestling tournament on Saturday at Escondido high school. We noticed this man setting up something. We asked what he was selling and he told us pizza. He had a nice set up.. looked like a wood fire oven. Later in that day we decided to order a pizza with a few of his toppings. Kinda of pricey but let me tell you well worth the money. Great size pizza, fresh toppings and oh so delicious!! This to me was better than any papa johns or domino's. I ended up buying him a coffee. This dude was super cool and awesome. I would highly recommend his services",
     star: 5,
-    name: "Steven B.",
+    name: "- Steven B.",
   },
   {
     text: "We just had them at our Office for our Thanksgiving party-ish thing.  It was tremendous!  I love their pizza, everything is so fresh, and hot.  We just had the pizza's and salad.  I hear they have great cheesesteaks.  Maybe we can get those next time.  They came set up and fed over 100 people in like 2 hours.   Their crew was organized and on it!  Thank you boss and thank you DropOuts Pizza.",
     star: 5,
-    name: "Lisa M.",
+    name: "- Lisa M.",
   },
   {
     text: "Used them before covid for work lunches.   Now using them for a surprise going away party with Wings and Pizza.   Try their new Ghost kitchen at doods deli if you aren't going to hire them for n event.  It's totally worth it",
     star: 5,
-    name: "L M.",
+    name: "- L M.",
   },
   {
     text: "Fredo is the best. He provides amazing service and even more amazing food with all fresh ingredients.",
     star: 5,
-    name: "Alondra A."
+    name: "- Alondra A."
   },
   {
     text:"So we decided to drive by our favorite brewery's second location and seen they were open. ( Never know lately) and Dropout Pizza was serving. Very light and tasty crust with fresh ingredients. We ordered a second one. Yeah, that good. Expect a nice small pie here. So you can order a few different ones. 5 stars.",
     star: 5,
-    name: "Jim E."
+    name: "- Jim E."
   },
   {
     text:"If you're looking for a great Pizza place in Escondido and nearby areas, i'd highly recommend to go to Dropout Pizza! The pizza is delicious and the quality is superb! Don't miss out on this place!",
     star:5,
-    name:"Paul D."
+    name:"- Paul D."
 
   },
   {
     text:"I used Fredo and Dropout for a dealer event we sponsored last October. They were a hit! Everyone got 3rds, the pizzas were amazing. Very friendly service, showed up on time at our location, and got wood fired pizzas going right away. We will definitely use them again- without question. I usually do street tacos, but this was much more popular with my dealers. Thanks again Fredo!",
     star: 5,
-    name: "Sam S."
+    name: "- Sam S."
   },
 ];
 
 
+for (let i =0; i<reviews.length; i++){
+  console.log(reviews[i].text.length)
+}
+
 
 let reviewGenerator = function(){
+
 let randomReview = reviews[Math.floor(Math.random()*reviews.length)]
+if(randomReview.star ===5){
+  starContainer.innerHTML = `<img class= "img-fluid" id ="star-target"src="http://pluspng.com/img-png/star-png-star-vector-png-transparent-image-2000.png">
+  <img class= "img-fluid" id ="star-target"src="http://pluspng.com/img-png/star-png-star-vector-png-transparent-image-2000.png">
+  <img class= "img-fluid" id ="star-target"src="http://pluspng.com/img-png/star-png-star-vector-png-transparent-image-2000.png">
+  <img class= "img-fluid" id ="star-target"src="http://pluspng.com/img-png/star-png-star-vector-png-transparent-image-2000.png">
+  <img class= "img-fluid" id ="star-target"src="http://pluspng.com/img-png/star-png-star-vector-png-transparent-image-2000.png">
+  `
+}
+
+nameTarget.textContent = randomReview.name
+nameTarget.style.opacity=0
+
+starContainer.style.opacity = 0
+if(randomReview.text.length < 315){
+  reviewBody.style.textAlign = "center"
+} else reviewBody.style.textAlign ="start"
 reviewBody.textContent = randomReview.text
+reviewBody.style.opacity = 0
 console.log(reviewBody)
 
 }
+let opacityFade = function(){
+  let reviewOpacity = Number(window.getComputedStyle(reviewBody).getPropertyValue("opacity"))
+  if (reviewBody.style.opacity <= 1){
+    reviewOpacity = reviewOpacity +.05
+    reviewBody.style.opacity = reviewOpacity
+  }
+  let nameOpacity = Number(window.getComputedStyle(nameTarget).getPropertyValue("opacity"))
+  if (nameTarget.style.opacity <= 1){
+    nameOpacity = nameOpacity +.05
+    nameTarget.style.opacity = nameOpacity
+  }
+  let starOpacity = Number(window.getComputedStyle(starContainer).getPropertyValue("opacity"))
+  if (starContainer.style.opacity <= 1){
+    starOpacity = starOpacity +.05
+    starContainer.style.opacity = starOpacity
+  }
 
-reviewGenerator();
+}
+setInterval(reviewGenerator, 3000)
+setInterval(opacityFade,100)
 
 // query params for yelp API
 // https://api.yelp.com/v3/businesses/ct27inqcT971zbGcxqp-qA/reviews
@@ -130,4 +172,3 @@ instagram();
 // console.log(gallery)
 // }
 
-console.log(typeof 42.1);
